1d826f1aed8c5742303cd01917a59f08777bacbc
Kelsey Grinde <kgrinde@macalester.edu>
2020-07-08 08:27:42 -0500

    initial commit

---
+++ R/toy_function.R
@@ -0,0 +1,15 @@
+#' A toy function.
+#'
+#' This function prints out a simple, not very useful, message.
+#'
+#' @return None. The function simply prints out a message.
+#'
+#' @examples
+#' toy_function()
+#'
+#' @seealso The general \code{\link[base]{print}} function.
+#'
+#' @export
+toy_function <- function() {
+  print("This is a function")
+}
---
+++ R/toyr.R
@@ -0,0 +1,9 @@
+#' toyr: A toy package.
+#'
+#' This package contains one simple, not very
+#' useful function. It was created for demonstration
+#' purposes.
+#'
+#' @docType package
+#' @name toyr
+NULL
