8c98acbdf8cff019d08e5756e1ae81e54fd49635
Kelsey Grinde <kgrinde@macalester.edu>
2020-07-17 14:22:59 -0500

    document data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example dataset.
+#'
+#' A dataset containing two randomly generated variables.
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'   \item{x}{first variable, random sample, without replacement, of integers from 1 to 10}
+#'   \item{y}{second variable, random sample of size 10 from standard normal distribution}
+#' }
+#'
+#' @source \code{example_df <- data.frame(x = sample(10), y = rnorm(10))}
+"example_df"
