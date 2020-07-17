c0cc3688351dfe5cf864c74465a940c84e1a9240
Kelsey Grinde <kgrinde@macalester.edu>
2020-07-17 14:24:28 -0500

    minor edit to data documentation

--- R/data.R
+++ R/data.R
@@ -4,8 +4,8 @@
 #'
 #' @format A data frame with 10 rows and 2 variables:
 #' \describe{
-#'   \item{x}{first variable, random sample, without replacement, of integers from 1 to 10}
-#'   \item{y}{second variable, random sample of size 10 from standard normal distribution}
+#'   \item{x}{first variable, sample without replacement of integers from 1 to 10}
+#'   \item{y}{second variable, a random sample of size 10 from standard normal distribution}
 #' }
 #'
 #' @source \code{example_df <- data.frame(x = sample(10), y = rnorm(10))}
